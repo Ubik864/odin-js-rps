36fe7de09b0fec51d063bac57c064a560cd19e88
Ubik864 <197319080+Ubik864@users.noreply.github.com.>
2025-02-19 01:27:08 -0600

    Start of game on Javascript

---
+++ main.js
@@ -0,0 +1,18 @@
+const choices = ["rock", "paper", "scissors"];
+
+function game() {
+    for(let i = 1; i <= 5; i++);
+}
+
+function playRound() {
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+}
+
+function playerChoice() {
+
+}
+
+function computerChoice() {
+
+}
