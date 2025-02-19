74eb2497d8207e3aeda10ff7faefe91750b0beb0
Ubik864 <197319080+Ubik864@users.noreply.github.com.>
2025-02-19 02:04:15 -0600

    Game can be played, and added more inputs

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const choices = ["rock", "paper", "scissors"];
 
 function game() {
     for (let i = 1; i <= 5; i++);
+    playRound();
 }
 
 function playRound() {
@@ -15,9 +16,22 @@ function playerChoice() {
         ("Type Rock Paper or Scissors");
     }
     input = input.toLowerCase();
+    let check = vaildateInput();
+
+ return input;
+}
+
+function vaildateInput(choice) {
+    if(choices.includes(choice)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function computerChoice() {
     return choices[Math.floor(Math.random()*choices.length)]
 
 }
+
+game();
